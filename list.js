2cfca81147cbfb647407f1b520f020704cd75f78
aaa@aa.com <aaa@aa.com>
2023-07-06 09:27:24 +0900

    프로젝트 완

--- list.js
+++ list.js
@@ -32,3 +32,62 @@ try {
   }
 }
 
+function printContacts(contacts) {
+  const $parent =$('#tbody');
+  for(c of contacts) {
+    const html = `
+      <tr>
+        <td>${c.no}</td>
+        <td><a href='read.html?no=${c.no}'>${c.name}</a></td>
+        <td>${c.tel}</td>
+        <td>${c.address}</td>
+      </tr>
+    `;
+    $parent.append(html);
+  }
+}
+
+// pagination에 필요한 prev, start, end, next, pageno를 리턴하는 함수
+
+// getPagination(result)->result에서 pageno, pagesizee, tatalcount를 꺼내는 문법
+// 구조분해할당
+function getPagination({pageno, pagesize, totalcount, blockSize=5}) {
+  //페이지의 개수 계산
+  const countOfPage = Math.ceil(totalcount/pagesize);
+  
+  //prev, start, end, next를 계산한 다음 목록의 끝에 도달한 경우 end, next를 변경
+  const prev = Math.floor((pageno-1)/blockSize)*blockSize;
+  const start = prev+1;
+  let end = prev + blockSize;
+  let next = end + 1;
+  if(end>=countOfPage) {
+    end = countOfPage;
+    next = 0;
+  }
+  // 구조분해할당 : 객체 ->변수로 분해, 변수를 모아서 객체를 생성
+  // return{prev:prev, start:start, end:end, next:next, pageno:pageno};
+  return {prev, start, end, next, pageno};
+}
+
+function printPagination({prev, start, end, next, pageno}) {
+  const $parent = $('#pagination');
+  if(prev>0) {
+    const html =
+    `<li class="page-item"><a href="list.html?pageno=${prev}" class="page-link">이전으로</a></li>`;
+    $parent.append(html);
+  }
+  for(let i=start; i<=end; i++) {
+    let className = 'page-item';
+    if(i===pageno) {
+      className = 'page-item active'
+    }
+    const html =
+    `<li class="${className}"><a href="list.html?pageno=${i}" class="page-link">${i}</a></li>`;
+    $parent.append(html);
+  }
+  if(next>0) {
+    const html =
+    `<li class="page-item"><a href="list.html?pageno=${next}" class="page-link">다음으로</a></li>`;
+    $parent.append(html);
+  }
+}
